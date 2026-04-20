a41e2a90fa7e89b0ebbd83699662ec5c4339d9a5
Noa Himesaka <himesaka@noa.codes>
2026-04-20 16:18:39 +0900

    t2linux changes

--- data/eos/scripts/chrooted_cleaner_script.sh
+++ data/eos/scripts/chrooted_cleaner_script.sh
@@ -117,7 +117,7 @@ _clean_offline_packages(){
         ## Calamares EndeavourOS
         $(pacman -Qq | grep calamares)        # finds calamares related packages
         ckbcomp
-	
+
     )
 
     pacman -Rsn --noconfirm "${packages_to_remove[@]}"
@@ -243,6 +243,12 @@ Main() {
     _clean_up
     _run_hotfix_end
     _show_info_about_installed_system
+
+    # Fix Wi-Fi by using iwd
+    cat <<EOF | sudo tee /etc/NetworkManager/conf.d/wifi_backend.conf
+[device]
+wifi.backend=iwd
+EOF
 
     # Remove pacnew files
     find /etc -type f -name "*.pacnew" -exec rm {} \;
--- data/eos/scripts/cleaner_script.sh
+++ data/eos/scripts/cleaner_script.sh
@@ -33,21 +33,10 @@ _CopyFileToTarget() {
 }
 
 _manage_broadcom_wifi_driver() {
-    local pkgname=broadcom-wl
     local targetfile=/tmp/$chroot_path/tmp/$pkgname.txt
 
-    # detecting broadcom hardware
-    if lsmod | grep -q "brcmfmac\|b43\|wl" \
-      || lspci -nn | grep -qi "14e4:43"; then
-
-        # check in addition if  broadcom-wl is installed on the live-system
-        if pacman -Q broadcom-wl &>/dev/null; then
-            echo "yes" > "$targetfile"
-        else
-            echo "Broadcom hardware found, but broadcom-wl not installed in live system" >&2
-        fi
-
-    fi
+    # broadcom-wl fucks up brcmfmac so hardcode no
+    echo "no" > $targetfile
 }
 
 _copy_files(){
